79813a19b3de2e0a95f904e8542db9fca7b2349f
bradley121901 <bradley121901@gmail.com>
2025-05-31 11:45:51 -0400

    screen set up

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+import random
+
+screen = turtle.Screen()
+screen.setup(800, 400)
+screen.title("Turtle Racing Game")
+screen.bgcolor(0, 0.5, 0)
+
+while True:
+    screen.update()
+
